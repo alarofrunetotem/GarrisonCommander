095e07f970bfbe55bc5bfec8780fadd40326db71
Alar of Daggerspine <alar@aspide.it>
2016-06-03 19:30:09 +0200

    Data update

--- wowhead.sh
+++ wowhead.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)
 echo -e "local me,ns = ...\n" >wowhead.lua
+date +"ns.wowhead_update=%s" >>wowhead.lua
 ../wowhelpers/GCAllRewards.php >>wowhead.lua
 #../wowhelpers/GCGearTokens.php >>wowhead.lua
