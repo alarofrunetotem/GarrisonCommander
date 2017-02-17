8e49963447527bb6bbbb04f614db7c8001182bdb
Alar of Runetotem <alar@aspide.it>
2017-02-17 11:45:49 +0100

    Added salvage items

--- wowhead.sh
+++ wowhead.sh
@@ -3,4 +3,4 @@ cd $(dirname $0)
 echo -e "local me,ns = ...\n" >wowhead.lua
 date +"ns.wowhead_update=%s" >>wowhead.lua
 ../wowhelpers/GCAllRewards.php >>wowhead.lua
-#../wowhelpers/GCGearTokens.php >>wowhead.lua
+../wowhelpers/GCSalvage.php >>wowhead.lua
