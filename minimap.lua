591542e3a83a8a70ca7f5dc4cdaa51c07a7a5ab5
Alar of Runetotem <alar@aspide.it>
2023-05-15 09:57:48 +0200

    Removed chat spam and fixed visualization

--- minimap.lua
+++ minimap.lua
@@ -18,56 +18,54 @@ local descriptions={
 }
 local function addTooltip(d,key,message)
   if (d==message) then return end
-  print(UnitLevel('player'))
   GameTooltip:AddLine(key .. " " .. message)
 end
+print("GCB",me,gc,IsAddOnLoaded(gc),gb,IsAddOnLoaded(gb))
 if (me ==  gc and  not IsAddOnLoaded(gb) or
     me ==  gb and  not IsAddOnLoaded(gc)
 ) then
-
+    print("Hooking tooltip")
      ExpansionLandingPageMinimapButton:HookScript("OnEnter",
      function(this)
       local d=this.description
-        if GarrisonLandingPage then
-          addTooltip(d,CTRL_KEY_TEXT,garrison)
-          addTooltip(d,SHIFT_KEY_TEXT,orderhall)
-          addTooltip(d,CTRL_KEY_TEXT .. '-' .. SHIFT_KEY_TEXT,champion)
-          if C_PlayerInfo.IsExpansionLandingPageUnlockedForPlayer(LE_EXPANSION_DRAGONFLIGHT) then addTooltip(d,CTRL_KEY_TEXT .. '-' .. ALT_KEY_TEXT,sanctum) end
-        end
+        addTooltip(d,CTRL_KEY_TEXT,garrison)
+        addTooltip(d,SHIFT_KEY_TEXT,orderhall)
+        addTooltip(d,CTRL_KEY_TEXT .. '-' .. SHIFT_KEY_TEXT,champion)
+        if C_PlayerInfo.IsExpansionLandingPageUnlockedForPlayer(LE_EXPANSION_DRAGONFLIGHT) then addTooltip(d,CTRL_KEY_TEXT .. '-' .. ALT_KEY_TEXT,sanctum) end
         GameTooltip:Show()
     end
     )
     ExpansionLandingPageMinimapButton:HookScript("OnClick",
       function (this,button)
-        if GarrisonLandingPage then
-         local shown=GarrisonLandingPage:IsShown()
-         local actual=GarrisonLandingPage.garrTypeID
-         local requested=C_Garrison.GetLandingPageGarrisonType()
-         local original=requested
-         local shift,ctrl,alt=IsShiftKeyDown(),IsControlKeyDown(),IsAltKeyDown()
-         print("KEYS",
-         shift and "shift" or "",
-         ctrl and "ctrl" or "",
-         alt and "alt" or ""
-         )
-         if ctrl then
-            if alt then
-              requested=LE_GARRISON_TYPE_9_0
-            elseif shift then
-              requested=LE_GARRISON_TYPE_8_0
-            else
-              requested=LE_GARRISON_TYPE_6_0
-            end
-         elseif shift then
-            requested=LE_GARRISON_TYPE_7_0
-         end
-         if InCombatLockdown() then return end
-         print("Check",descriptions[requested],'vs',descriptions[actual])
-         if shown and actual ~= requested and requested <= actual then
-           print("Show ",descriptions[requested],'vs',descriptions[actual])
-            ShowGarrisonLandingPage(requested);
-         end
+        local shift,ctrl,alt=IsShiftKeyDown(),IsControlKeyDown(),IsAltKeyDown()
+        local requested=0
+        print (shift,ctrl,alt)
+        if ctrl then
+          if alt then
+            requested=LE_GARRISON_TYPE_9_0
+          elseif shift then
+            requested=LE_GARRISON_TYPE_8_0
+          else
+            requested=LE_GARRISON_TYPE_6_0
+          end
+        elseif shift then
+          requested=LE_GARRISON_TYPE_7_0
+        else
+          return
+        end
+        if not GarrisonLandingPage then
+          Garrison_LoadUI()
+        end
+          if InCombatLockdown() then return end
+        local actual=GarrisonLandingPage.garrTypeID
+        local original=requested
+        if ExpansionLandingPage and ExpansionLandingPage:IsShown() then
+           ExpansionLandingPage:Hide()
+        end
+        if actual ~= requested then
+          ShowGarrisonLandingPage(requested);
+          
        end
-      end
+     end
     )
 end
